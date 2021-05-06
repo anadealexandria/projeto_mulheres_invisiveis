f8e635b1b6b436c27acf185a05996ffc60e75d7c
anabeatrizjp <beatrizdealexandria@gmail.com>
2021-05-05 21:29:34 -0300

    modelos doador, doação e voluntaria

--- src/model/Doador.java
+++ src/model/Doador.java
@@ -2,4 +2,39 @@ package model;
 
 public class Doador {
 
+		private String nome;
+		private  String cpfCnpj;
+		private double doacao;
+		
+		public Doador() {
+}
+		public Doador (String nome, String cpfCnpj, double doacao ) {
+			this.nome = nome;
+			this.cpfCnpj = cpfCnpj;
+			this.doacao = doacao;
+		}
+		
+		public String getnome() {
+			return nome;
+		}
+		
+		public void setnome(String nome) {
+			this.nome = nome;
+		}
+		
+		public String getcpfCnpj() {
+			return cpfCnpj;
+		}
+		
+		public void setcpfCnpj(String cpfCnpj) {
+			this.cpfCnpj = cpfCnpj;
+		}
+		
+		public double getdoaçao() {
+			return doacao;
+		}
+		
+		public void setdoaçao(double doacao) {
+			this.doacao = doacao;
+		}
 }
--- src/model/Voluntaria.java
+++ src/model/Voluntaria.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Voluntaria {
-
+	
+	private String nome;
+	private String telefone;
+	private String disponibilidadeTempo;
+	
+	public Voluntaria() {
+		
+	}
+	public Voluntaria (String nome, String telefone, String disponibilidadeTempo) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.disponibilidadeTempo = disponibilidadeTempo;
+	}
+	
+	public String getnome() {
+		return nome;
+	}
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	public String gettelefone() {
+		return telefone;
+	}
+	public void settelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getdisponibilidadeTempo() {
+		return disponibilidadeTempo;
+	}
+	public void setdisponibilidadeTempo() {
+		this.disponibilidadeTempo = disponibilidadeTempo;
+	}
 }
